ef2dfa1eb255ab8dade90910b47773b5dbefadab
Richard Cancino Vilchez <richard.cancinov@gmail.com>
2020-10-01 23:22:42 -0500

    Add screen bootom

--- app/src/main/java/com/example/vicky/androidui/HomeActivity.kt
+++ app/src/main/java/com/example/vicky/androidui/HomeActivity.kt
@@ -1,12 +1,52 @@
 package com.example.vicky.androidui
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.example.vicky.androidui.R
+import kotlinx.android.synthetic.main.home_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_main)
+        setContentView(R.layout.home_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
     }
+
+    private val mOnNavigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.item_stores -> {
+
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.item_favorites -> {
+
+
+                        return@OnNavigationItemSelectedListener true
+                    }
+
+                    R.id.item_orders -> {
+
+
+                        return@OnNavigationItemSelectedListener true
+
+                    }
+
+                    R.id.item_shopping -> {
+
+
+                        return@OnNavigationItemSelectedListener true
+
+                    }
+
+                    R.id.item_profile -> {
+
+                        return@OnNavigationItemSelectedListener true
+
+                    }
+                }
+                false
+            }
 }
