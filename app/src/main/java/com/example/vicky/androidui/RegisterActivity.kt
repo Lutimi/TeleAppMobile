6d47287ddf85ba49af86aed82481c7947c1264a6
Richard Cancino Vilchez <richard.cancinov@gmail.com>
2020-10-01 22:17:57 -0500

    Add screen register

---
+++ app/src/main/java/com/example/vicky/androidui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vicky.androidui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.vicky.androidui.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_main)
+    }
+}
